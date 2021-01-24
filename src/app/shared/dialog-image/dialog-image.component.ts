9dff21d5b52707790135b69ef5e915a3b447531e
roman <roman.lapacherie@gmail.com>
2021-01-24 21:34:18 +0000

    fix linting

--- src/app/shared/dialog-image/dialog-image.component.ts
+++ src/app/shared/dialog-image/dialog-image.component.ts
@@ -1,6 +1,6 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/storage';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -12,7 +12,7 @@ export class DialogImageComponent {
   public imageUrl$: Observable<string>;
 
   constructor(private storage: AngularFireStorage,
-              @Inject(MAT_DIALOG_DATA) private data: { folder: string, image: string}) {
+              @Inject(MAT_DIALOG_DATA) private data: { folder: string, image: string }) {
     this.imageUrl$ = this.storage.ref(`${this.data.folder}/${this.data.image}.jpg`).getDownloadURL();
   }
 }
