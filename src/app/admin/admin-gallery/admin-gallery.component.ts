8d482b4fc7c88a680e7f7c21c7e02f30625b3b24
Roman Lapacherie <roman.lapacherie@gmail.com>
2025-01-29 21:29:50 +0000

    fix

--- src/app/admin/admin-gallery/admin-gallery.component.ts
+++ src/app/admin/admin-gallery/admin-gallery.component.ts
@@ -1,44 +1,59 @@
-import { CdkDropList } from "@angular/cdk/drag-drop";
-import { AsyncPipe, NgForOf, NgIf, NgOptimizedImage } from "@angular/common";
+import { CdkDropList } from '@angular/cdk/drag-drop';
+import { AsyncPipe, NgForOf, NgIf, NgOptimizedImage } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { arrayUnion, collection, doc, docData, Firestore, updateDoc } from "@angular/fire/firestore";
-import { getDownloadURL, ref, Storage, uploadBytes } from '@angular/fire/storage';
-import { MatCardModule } from "@angular/material/card";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { ActivatedRoute } from "@angular/router";
+import {
+  arrayUnion,
+  collection,
+  CollectionReference,
+  doc,
+  Firestore,
+  updateDoc,
+} from '@angular/fire/firestore';
+import {
+  getDownloadURL,
+  ref,
+  Storage,
+  uploadBytes,
+} from '@angular/fire/storage';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { ActivatedRoute } from '@angular/router';
+import { docData } from 'rxfire/firestore';
 import { map, switchMap } from 'rxjs/operators';
-import { GalleryData } from "../../shared/gallery/GalleryData";
+import { GalleryData } from '../../shared/gallery/GalleryData';
 
 @Component({
-    selector: 'osef-admin-gallery',
-    templateUrl: './admin-gallery.component.html',
-    styleUrls: ['./admin-gallery.component.scss'],
-    imports: [
-        NgIf,
-        NgForOf,
-        AsyncPipe,
-        MatGridListModule,
-        MatCardModule,
-        CdkDropList,
-        NgOptimizedImage
-    ]
+  selector: 'osef-admin-gallery',
+  templateUrl: './admin-gallery.component.html',
+  styleUrls: ['./admin-gallery.component.scss'],
+  imports: [
+    NgIf,
+    NgForOf,
+    AsyncPipe,
+    MatGridListModule,
+    MatCardModule,
+    CdkDropList,
+    NgOptimizedImage,
+  ],
 })
 export class AdminGalleryComponent {
-
   firestore: Firestore = inject(Firestore);
   storage: Storage = inject(Storage);
-  collection = collection(this.firestore, 'galleries');
+  collection = collection(
+    this.firestore,
+    'galleries'
+  ) as CollectionReference<GalleryData>;
 
   document$ = this.activatedRoute.params.pipe(
     map((params) => params['id']),
-    switchMap((id) => docData<GalleryData>(doc(this.collection, id), {idField: 'id'})),
+    switchMap((id) =>
+      docData<GalleryData>(doc(this.collection, id), { idField: 'id' })
+    )
   );
 
-  constructor(private activatedRoute: ActivatedRoute) {
-  }
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   uploadImage(id: string, event: Event) {
-
     // @ts-ignore
     const image = (event.target as HTMLInputElement).files[0];
 
@@ -49,8 +64,8 @@ export class AdminGalleryComponent {
     uploadBytes(storageRef, image).then(() => {
       getDownloadURL(storageRef).then((link) => {
         updateDoc(doc(this.collection, id), {
-          photos: arrayUnion({name: image.name, url: link})
-        })
+          photos: arrayUnion({ name: image.name, url: link }),
+        });
       });
     });
   }
