987d9c2a27907478213d19c9cc1b5cad3e594838
romanlp <roman.lapacherie@gmail.com>
2026-07-11 20:00:40 +0100

    remove @angular/fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,4 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { getAnalytics, provideAnalytics as provideAnalyticsLegacy } from '@angular/fire/analytics';
-import {
-  initializeApp as initializeAppLegacy,
-  provideFirebaseApp as provideFirebaseAppLegacy,
-} from '@angular/fire/app';
-import {
-  initializeAppCheck,
-  provideAppCheck as provideAppCheckLegacy,
-  ReCaptchaV3Provider,
-} from '@angular/fire/app-check';
-import { getFirestore, provideFirestore as provideFirestoreLegacy } from '@angular/fire/firestore';
-import { getPerformance, providePerformance as providePerformanceLegacy } from '@angular/fire/performance';
-import { getStorage, provideStorage as provideStorageLegacy } from '@angular/fire/storage';
 import { provideRouter } from '@angular/router';
 import { initializeApp } from 'firebase/app';
 import { environment } from '../environments/environment';
@@ -37,18 +24,5 @@ export const appConfig: ApplicationConfig = {
     providePerformance(),
     provideFirestore(),
     provideStorage(),
-
-    // Old Angularfire configuration for compatibility with existing code
-    provideFirebaseAppLegacy(() => initializeAppLegacy(environment.firebase)),
-    provideFirestoreLegacy(() => getFirestore()),
-    provideAppCheckLegacy(() =>
-      initializeAppCheck(undefined, {
-        provider: new ReCaptchaV3Provider(environment.recaptcha),
-        isTokenAutoRefreshEnabled: true,
-      }),
-    ),
-    providePerformanceLegacy(() => getPerformance()),
-    provideStorageLegacy(() => getStorage()),
-    provideAnalyticsLegacy(() => getAnalytics()),
   ],
 };
